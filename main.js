a675c7bc05cf0ca2521fc75563f47466b5bd8fc4
alexisllamas <alexisnavarro23@gmail.com>
2017-03-13 22:51:03 +0000

    Add the context menu to the input

--- main.js
+++ main.js
@@ -7,6 +7,14 @@ const BrowserWindow = electron.BrowserWindow;
 const path = require('path');
 const url = require('url');
 
+require('electron-context-menu')({
+  prepend: (params) => [{
+    label: 'Rainbow',
+    // only show it when right-clicking images
+    visible: params.mediaType === 'image'
+  }]
+});
+
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
