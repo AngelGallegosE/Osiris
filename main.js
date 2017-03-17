9b882929a1e2d332880d311e126b403900ea4589
alexisllamas <alexisnavarro23@gmail.com>
2017-03-17 19:38:23 +0000

    Yay!

--- main.js
+++ main.js
@@ -23,7 +23,12 @@ let mainWindow;
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    resizable: false,
+    title: 'Osiris'
+  });
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
