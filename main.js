45a1679825bf483b2c4d109f2a4771c905056074
AngelGallegos <7angelgallegos@gmail.com>
2017-03-17 18:10:37 -0600

    Space between url and status icons

--- main.js
+++ main.js
@@ -27,9 +27,8 @@ function createWindow () {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
-    resizable: false,
+    resizable: true,
     title: 'Osiris',
-    fullscreenable: false,
   });
 
   // and load the index.html of the app.
