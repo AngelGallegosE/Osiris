a8c5a49d168008d45862bff557cb4cf171f6e132
AngelGallegos <7angelgallegos@gmail.com>
2017-03-20 16:39:39 -0600

    bugfix_tooltip_problem

--- app/components/Status.js
+++ app/components/Status.js
@@ -3,12 +3,12 @@ import React from 'react';
 const Status = ({url, title, validUrl, working, status, progressBar}) => (
   <div className="status unselectable">
     <a href="#" onClick={() => { pdfFunctions.openURL(url); }}>
-      {title} - 
-      {validUrl?
-        <i data-tooltip='Valid Link' className="fa fa-check greenIconColor" aria-hidden="true"> </i>:
-        <i data-tooltip='Invalid Link' className="fa fa-ban redIconColor" aria-hidden="true"> </i>
-      }
-    </a> 
+      {title} 
+    </a> - 
+    {validUrl?
+      <i data-tooltip='Valid Link' className="fa fa-check greenIconColor" aria-hidden="true"> </i>:
+      <i data-tooltip='Invalid Link' className="fa fa-ban redIconColor" aria-hidden="true"> </i>
+    }
     <div className={progressBar!==0?status==1 ? 'greenStatus' : 'redStatus':''}></div>
     {validUrl && progressBar!==0 && status===0? 'Loading Page':''}
   </div>
