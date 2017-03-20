fa33c426856b1bee2a2974af3bf5c7507b92e445
AngelGallegos <7angelgallegos@gmail.com>
2017-03-20 13:07:22 -0600

    bugfix/progress_bar

--- app/components/ToPDF.js
+++ app/components/ToPDF.js
@@ -104,7 +104,8 @@ export default class ToPDF extends React.Component {
       progressBar: 0.1,
     }, () => {
       this.updateArrayDomains();
-      const numberOfLineBreaks = (this.state.urls.match(/\n/g).filter(e=>this.isValidURL(e)) || []).length + 1;
+      const numberOfValidUrls = this.state.urls.split('\n').filter(e=>this.isValidURL(e)).length;
+      console.log(numberOfValidUrls);
       let linksDownloaded = 0;
       if (this.state.urls != '') {
         this.setState({
@@ -117,7 +118,7 @@ export default class ToPDF extends React.Component {
               let final = this.state.arrayDomains;
               final[index].status = 1;
               linksDownloaded++;
-              this.setProgressBar(linksDownloaded, numberOfLineBreaks);
+              this.setProgressBar(linksDownloaded, numberOfValidUrls);
               this.setState({
                 arrayDomains: final,
                 working: '',
