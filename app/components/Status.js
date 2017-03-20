1c0648ca44d4f622ecd0837c754764dc87946d53
AngelGallegos <7angelgallegos@gmail.com>
2017-03-20 14:49:11 -0600

    bugfix_scroll_fixed

--- app/components/Status.js
+++ app/components/Status.js
@@ -4,7 +4,7 @@ class Status extends React.Component {
   render() {
     return (
       <div className="status unselectable"> 
-        <a href="#" onClick={() => {pdfFunctions.openURL(this.props.url)}}>{this.props.title}</a> - {this.props.validUrl?<i data-tooltip='Valid Link' className="fa fa-check greenIconColor" aria-hidden="true"> </i>:<i data-tooltip='Invalid Link' className="fa fa-ban redIconColor" aria-hidden="true"> </i>} 
+        <a href="#" onClick={() => {pdfFunctions.openURL(this.props.url)}}>{this.props.title}- {this.props.validUrl?<i data-tooltip='Valid Link' className="fa fa-check greenIconColor" aria-hidden="true"> </i>:<i data-tooltip='Invalid Link' className="fa fa-ban redIconColor" aria-hidden="true"> </i>} </a> 
         <div className={this.props.progressBar!==0?this.props.status==1 ? 'greenStatus' : 'redStatus':''}></div>  
         {this.props.validUrl?this.props.working:''}
       </div>
