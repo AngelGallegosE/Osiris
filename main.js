1b704eacd61a4143b09894bdea6511fbdb0d90c9
AngelGallegos <7angelgallegos@gmail.com>
2017-03-21 11:52:36 -0600

    Background color in BrowserWindows

--- main.js
+++ main.js
@@ -22,6 +22,7 @@ function createWindow () {
   // Create the browser window.
 
   mainWindow = new BrowserWindow({
+    backgroundColor: '#ECF0F1',
     width: 800,
     height: 600,
     resizable: true,
